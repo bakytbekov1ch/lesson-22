b0333761e98b40b8b508d810ddb0242466e28494
Eliek <kalmamatovelibek32@gmail.com>
2024-03-27 11:38:16 +0600

    API lesson-22

---
+++ index.js
@@ -0,0 +1,32 @@
+const API = "https://65ab6a1efcd1c9dcffc659a4.mockapi.io/api/v1/services";
+const result = document.querySelector(".result");
+
+async function getService() {
+  try {
+    const res = await fetch(API);
+
+    const data = await res.json();
+
+    console.log(data);
+    showService(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getService();
+
+function showService(data) {
+  result.innerHTML = data
+    .map((item) => {
+      return `
+            <div class="product">
+                <img src="${item.image}" alt="images"/>
+                <p>${item.name}</p>
+                <p>${item.category}</p>
+                <p>${item.price}</p>
+            </div>
+        `;
+    })
+    .join("");
+}
